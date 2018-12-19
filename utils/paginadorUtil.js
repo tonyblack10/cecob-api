9bb5ac0f84c7c21d69c257ef4e31109d12abcc7e
tonyblack10 <tonyblack10@gmail.com>
2018-12-18 21:37:16 -0300

    Inserção de mais atributos no objeto de paginação.

--- utils/paginadorUtil.js
+++ utils/paginadorUtil.js
@@ -4,17 +4,17 @@ const realizaPaginacao = (Model, opcoes = {}, numeroDaPagina = 0) => {
   opcoes.offset = offset;
   const pagina = {};
   return Model.findAll(opcoes)
-  .then(resultado => {
-    const where = opcoes.where || {};
-    pagina.conteudo = resultado;
-    return Model.count({where})
-  })
-  .then(totalDeElementos => {
-    pagina.totalDeElementos = totalDeElementos;
-    return pagina;
-  });
+    .then(resultado => {
+      const where = opcoes.where || {};
+      pagina.conteudo = resultado;
+      return Model.count({where})
+    })
+    .then(totalDeElementos => {
+      pagina.totalDeElementos = totalDeElementos;
+      pagina.totalDePaginas = Math.ceil(totalDeElementos / process.env.QTD_POR_PAGINA);
+      pagina.paginaAtual = numeroDaPagina;
+      return pagina;
+    });
 };
 
-module.exports = {
-  realizaPaginacao
-};
+module.exports = { realizaPaginacao };
